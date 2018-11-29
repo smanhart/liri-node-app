f24beb8f40d7ff09eb70dc6f14348a86cd2614cc
Lucky Penny <41957862+smanhart@users.noreply.github.com>
2018-11-28 18:26:45 -0700

    spotify api is working

--- liri.js
+++ liri.js
@@ -1,1 +1,34 @@
 require("dotenv").config();
+var Spotify = require('node-spotify-api');
+var keys = require("./keys.js")
+var spotify = new Spotify(keys.spotify);
+var command = process.argv[2];
+
+if (command === "spotify-this-song") {
+
+    var trackName = process.argv.slice(3).join(" ");
+    spotify.search({ type: 'track', query: trackName }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        console.log(`
+        Artist: ${data.tracks.items[0].album.artists[0].name}
+        Song Title: ${data.tracks.items[0].name}
+        Preview Link: ${data.tracks.items[0].preview_url}
+        Album: ${data.tracks.items[0].album.name}`);
+    
+    });
+
+
+}
+
+
+//   spotify
+//   .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+//   .then(function(data) {
+//     console.log(data); 
+//   })
+//   .catch(function(err) {
+//     console.error('Error occurred: ' + err); 
+//   });
